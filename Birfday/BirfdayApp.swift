b05ed72ad700ae47a21dc9d61ef6e4a8348c8500
alegonshu <alegonshu@gmail.com>
2024-05-28 22:00:33 -0400

    Initial Commit

---
+++ Birfday/BirfdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirfdayApp.swift
+//  Birfday
+//
+//  Created by Alejandro Gonzalez on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BirfdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
